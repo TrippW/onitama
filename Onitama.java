745cbc2c1c3e46311289534be555b702d8bb4f79
TrippW <TrippW@users.noreply.github.com>
2017-05-18 15:02:42 -0500

    Fixed Player to not include board in creation, but to be set with funciton later

--- Onitama.java
+++ Onitama.java
@@ -10,13 +10,22 @@ public class Onitama
 
  public Onitama()
  {
+  GUI g = new GUI();
+
   Card cards[] = Deck.draw();
-  Board b = new Board(cards[2]);
-  Player p1 = new Player(new Card[]{cards[0],cards[3]}, false, Board.red, b);
-  Player p2 = new Player(new Card[]{cards[1],cards[4]}, false, Board.blue, b);
+  Player p1 = new Player(new Card[]{cards[0],cards[3]}, false, Board.red);
+  Player p2 = new Player(new Card[]{cards[1],cards[4]}, false, Board.blue);
 
+  Board b = new Board(cards[2], p1, p2);
 
-  b.display();
+  p1.setBoard(b);
+  p2.setBoard(b);
+
+  while(b.checkWin() && false)
+  {
+   b.move(b.getMove());
+   b.switchPlayer();
+  }
 
   for(int i = 0;i < cards.length;i++)
    System.out.println(cards[i].name);
--- Player.java
+++ Player.java
@@ -1,14 +1,22 @@
 public class Player
 {
-
  Card[] cards;
  Board board;
  int color;
 
- public Player(Card[] moves, boolean isComputer, int playerColor, Board b)
+ public Player(Card[] moves, boolean isComputer, int playerColor)
  {
-  board = b;
   cards = moves;
   color = playerColor;
+ }
+
+ public void setBoard(Board b)
+ {
+  board = b;
+ }
+
+ public int[] getMove(int selectedCard, int option)
+ {
+  return cards[selectedCard].moves[option];
  }
 }
