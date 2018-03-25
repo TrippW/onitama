e740b2ed5ecd253bad2158e29da56b32a202d67a
Emanuele F <emanueleffe@users.noreply.github.com>
2018-03-25 18:01:24 +0200

    added "Help" case
    added "Help" case with a MessageDialog to read instructions

--- MenuListener.java
+++ MenuListener.java
@@ -6,10 +6,18 @@ public class MenuListener implements ActionListener {
 	public MenuListener() {
 	}
 
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e)
+	{
+		final String instructions = "To win you need to eliminate your opponents king piece or get your king piece to the opponents center square.\n" +
+				"First, click on one of the two cards on your side of the board. Then click on the piece you want to move.\n" +
+				"You can change either the card or the piece you want to use that turn before finalizing your move.\nTo finalize " +
+				"the move, click on any of the yellow highlighted squares.\nYour piece will move there and you will swap the card " +
+				"you used for the card in the center.\nGame continues until there is a winner.";
 		String input = "";
 		int count = -2;
-		switch(e.getActionCommand()) {
+		switch(e.getActionCommand())
+		{
+
 		case "Difficulty":
 			System.out.println("Set Difficulty...");
 			while(count < 0 || count > 2) {
@@ -46,6 +54,9 @@ public class MenuListener implements ActionListener {
 		case "Reset":
 			System.out.println("Reseting...");
 			Onitama.reset();
+			break;
+		case "Help":
+			JOptionPane.showMessageDialog(null, instructions, "How to play", JOptionPane.INFORMATION_MESSAGE);
 			break;
 		}
 	}
